9c2a530701f64c76cfac7f371d2e18255e8b5a52
Leksiq <leksi@leksi.net>
2023-05-12 19:40:01 +0300

    =

--- WpfMarkupExtension/UniversalConverterParameter.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Net.Leksi.WpfMarkup;
-
-public class UniversalConverterParameter
-{
-    public string Selector { get; set; } = string.Empty;
-    public object? Parameter { get; set; }
-}
