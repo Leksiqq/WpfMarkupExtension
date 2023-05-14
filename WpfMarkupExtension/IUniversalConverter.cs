a99dd92a8c733a6031a454902a272ebec2394a6c
Leksiq <leksi@leksi.net>
2023-05-14 11:22:24 +0300

    =

--- WpfMarkupExtension/IUniversalConverter.cs
+++ WpfMarkupExtension/IUniversalConverter.cs
@@ -1,7 +1,24 @@
-﻿using System.Windows.Data;
+﻿using System;
+using System.Windows.Data;
 
 namespace Net.Leksi.WpfMarkup;
 
 public interface IUniversalConverter : IValueConverter, IMultiValueConverter
 {
+    public static object?[] SplitParameter(object? parameter)
+    {
+        if(parameter is Array array)
+        {
+            object[] res = new object[array.Length];
+            array.CopyTo(res, 0);
+            return res;
+        }
+        if(parameter is string str)
+        {
+            return str.Split('|');
+        }
+        return new object?[] { parameter };
+    }
+
+
 }
